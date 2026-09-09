1410ee298f8491605f968ff79e474d5984878139
Морозов В.В. <you@example.com>
2026-09-09 13:56:59 +0300

    1 task complete

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -21,15 +21,29 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
- 
+
+class Lecturer(Mentor):
+    pass
+
+class Reviewer(Mentor):
+    pass
+
+lecturer = Lecturer('Иван', 'Иванов')
+reviewer = Reviewer('Пётр', 'Петров')
+
+print(isinstance(lecturer, Mentor))   # True
+print(isinstance(reviewer, Mentor))   # True
+print(lecturer.courses_attached)      # []
+print(reviewer.courses_attached)      # []
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
- 
+
 cool_mentor = Mentor('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
- 
+
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
- 
+
 print(best_student.grades)
